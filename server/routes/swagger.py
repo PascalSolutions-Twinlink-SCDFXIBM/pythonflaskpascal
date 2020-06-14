273d0ac31c4a87e42814822416d776d7260a1d8d
Darwin Liew <liewdarwin@live.com>
2020-06-14 13:38:14 +0800

    dashboard, digital twin 3d js viewer

--- server/routes/swagger.py
+++ server/routes/swagger.py
@@ -3,7 +3,7 @@ from flask import jsonify, render_template
 from server import app
 
 @app.route("/swagger/api")
-def swagger_api():    
+def swagger_api():
     with open("public/swagger.yaml", "r") as f:
         content = f.read()
     return "<pre>"+content+"</pre>"
@@ -11,3 +11,12 @@ def swagger_api():
 @app.route("/explorer")
 def explorer():
     return render_template('swagger-ui/index.html')
+
+
+@app.route("/twin")
+def digitaltwin():
+    return render_template('swagger-ui/twin.html')
+
+@app.route("/twinbuilding")
+def twinbuilding():
+    return render_template('swagger-ui/twinbuilding.html')
